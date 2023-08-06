31dae0c333cf4459aff874e289203f6043a5f4ca
Erick Daniel Escoto Perez <erickdanielescoto@gmail.com>
2023-08-06 14:47:02 -0600

    anexo del index al codigo

--- Frontend/assets/controlodar.js
+++ Frontend/assets/controlodar.js
@@ -1,2 +1,31 @@
 //Josue Salomon
 //Erick Escoto 
+
+function ConfirmarCompra(){
+    console.log('Probasodjasd')
+    document.getElementById('detallesCompra').style.display='none'
+    document.getElementById('confirmarCompra').style.display='block'
+}
+
+function regresarConfirmacion(){
+    document.getElementById('detallesCompra').style.display='block'
+    document.getElementById('confirmarCompra').style.display='none'
+}
+
+function inicioSesion(){
+    document.getElementById('InicioSesion').style.display='block'
+    document.getElementById('Landing-page').style.display='none'
+}
+
+function regresoLanding(){
+    document.getElementById('InicioSesion').style.display='none'
+    document.getElementById('Landing-page').style.display='block'
+    document.getElementById('Registro').style.display='none'
+}
+
+function RegistroSesion(){
+    document.getElementById('InicioSesion').style.display='none'
+    document.getElementById('Landing-page').style.display='none'
+    document.getElementById('Registro').style.display='flex'
+
+}
