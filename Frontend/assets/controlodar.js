7659060d6dab9a0633038ff5f8cf6bb5db215835
Erick Daniel Escoto Perez <erickdanielescoto@gmail.com>
2023-08-26 05:49:33 -0600

    admin antes de irme a mimir

--- Frontend/assets/controlodar.js
+++ Frontend/assets/controlodar.js
@@ -78,19 +78,19 @@ function mostrarFactura(){
 function mostrarProductosAdmin(){
     document.getElementById('administradores_productos').style.display ="flex";
     document.getElementById('administradores').style.display ="none";
-    document.getElementById('adminIndex').style.display ="none";
+    document.getElementById('indexAdministradores').style.display ="none";
 }
 
 function mostrarEmpresas(){
     document.getElementById('administradores_productos').style.display ="none";
     document.getElementById('administradores').style.display ="flex";
-    document.getElementById('adminIndex').style.display ="none";
+    document.getElementById('indexAdministradores').style.display ="none";
 }
 
 function mostrarMotoristasAdmin(){
     document.getElementById('administradores_productos').style.display ="none";
     document.getElementById('administradores').style.display ="none";
-    document.getElementById('adminIndex').style.display ="flex";
+    document.getElementById('indexAdministradores').style.display ="flex";
 }
 
 //parte de josue
