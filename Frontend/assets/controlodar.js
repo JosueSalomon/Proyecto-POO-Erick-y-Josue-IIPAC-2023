f73955311424466a34cb0af62408117bdc9b0a70
Erick Daniel Escoto Perez <erickdanielescoto@gmail.com>
2023-08-26 19:06:30 -0600

    administradores funcional

--- Frontend/assets/controlodar.js
+++ Frontend/assets/controlodar.js
@@ -96,6 +96,7 @@ function mostrarIndexAdmin(){
     document.getElementById('productosAdministradores').style.display ="none";
     document.getElementById('indexAdministradores').style.display ="flex";
     document.getElementById('motoristasAdministradores').style.display ="none";
+    document.getElementById('loginAdministradores').style.display ="none";
 }
 function mostrarEmpresas(){
     document.getElementById('empresasAdministradores').style.display ="flex";
@@ -120,6 +121,78 @@ function mostrarOrdenes(){
     document.getElementById('indexAdministradores').style.display ="none";
     document.getElementById('motoristasAdministradores').style.display ="none";
 }
+function editarmotoristaAdmin(id){
+    document.getElementById(id).innerHTML =`
+    <div style=" width: 7%; display: inline-block; border-radius: 50%; overflow: hidden;" ><img style="height: 100%; object-fit: contain;" src="/Frontend/assets/img/adminJosue.jpg" alt=""></div>
+    <div style=" width: 25%; margin: 0px; display: flex; align-items: center; overflow: hidden;"><h4 style="margin: 0 2px;">Josue Isac Salomon Landa</h4></div>
+    <div style="width: 18%; text-align: center; margin: 0 2px; display: flex; align-items: center;"><h4>AAA-0101</h4></div>
+    <div style="width: 21%; text-align: center; margin: 0 2px; display: flex; align-items: center;"><h4>65</h4></div>
+    <div style="width: 18%; text-align: end;margin: 0 4px; display: flex; align-items: center;">
+        <select id="opcionesEstadoAdmin" style="border: none; height:40%; width: 65%; font-family: 'Ubuntu'; font-size: 15px; border-radius: 10px;">
+            <option value=true>Aprobado</option>
+            <option value=false>En espera</option>
+        </select>
+    </div>
+    <div style="width: 11%;; margin-right: 4px; display: flex; justify-content: space-evenly; align-items: center;">
+        <i onclick="confirmareditarmotoristaAdmin('${id}')" class="fa-solid fa-check" style="color:#06283D; font-size: 15px;"></i>
+        <i class="fa-solid fa-trash" style="color:#06283D; font-size: 15px;"></i>
+    </div>
+    `
+
+}
+
+function confirmareditarmotoristaAdmin(id){
+    estado=document.getElementById('opcionesEstadoAdmin').value;
+    console.log(estado)
+    document.getElementById(id).innerHTML =`
+    <div style=" width: 7%; display: inline-block; border-radius: 50%; overflow: hidden;" ><img style="height: 100%; object-fit: contain;" src="/Frontend/assets/img/adminJosue.jpg" alt=""></div>
+    <div style=" width: 25%; margin: 0px; display: flex; align-items: center; overflow: hidden;"><h4 style="margin: 0 2px;">Josue Isac Salomon Landa</h4></div>
+    <div style="width: 18%; text-align: center; margin: 0 2px; display: flex; align-items: center;"><h4>AAA-0101</h4></div>
+    <div style="width: 21%; text-align: center; margin: 0 2px; display: flex; align-items: center;"><h4>65</h4></div>
+    <div style="width: 18%; text-align: end;margin: 0 4px; display: flex; align-items: center;"><h4>Aprobado</h4></div>
+    <div style="width: 11%;; margin-right: 4px; display: flex; justify-content: space-evenly; align-items: center;">
+        <i onclick="editarmotoristaAdmin('${id}')" class="fa-solid fa-pen" style="color:#06283D; font-size: 15px;"></i>
+        <!-- <i class="fa-solid fa-trash" style="color:#06283D; font-size: 15px;"></i> -->
+    </div>
+    `
+
+}
+
+function editarProductoAdmin(id){
+    document.getElementById(id).innerHTML=`
+    <div style=" width: 7%;" ><img style="height: 100%; object-fit: contain;" src="/Frontend/assets/img/libro.png" alt=""></div>
+    <div style=" width: 25%; margin: 0px; display: flex; align-items: center; overflow: hidden;"><h4 style="margin: 0 2px;">
+    <input type="text" placeholder="Nombre" style="border: none; height="20px"">
+    </div>
+    <div style="width: 25%; text-align: center; margin: 0 2px; display: flex; align-items: center;">
+    <input type="text" placeholder="Categoria" style="border: none; height="20px"">
+    </div>
+    <div style="width: 18%; text-align: center; margin: 0 2px; display: flex; align-items: center;">
+    <input type="text" placeholder="Precio" style="border: none; height="20px"">
+    </div>
+    <div style="width: 18%; text-align: end;margin: 0 4px; display: flex; align-items: center;"></div>
+    <div style="width: 7%;; margin-right: 4px; display: flex; justify-content: space-around; align-items: center;">
+        <i onclick="confirmarEditarProductoAdmin('${id}')" class="fa-solid fa-check" style="color:#06283D; font-size: 15px;"></i>
+
+    </div>
+    `
+}
+
+function confirmarEditarProductoAdmin(id){
+    document.getElementById(id).innerHTML=`
+    <div style=" width: 7%;" ><img style="height: 100%; object-fit: contain;" src="/Frontend/assets/img/libro.png" alt=""></div>
+    <div style=" width: 25%; margin: 0px; display: flex; align-items: center; overflow: hidden;"><h4 style="margin: 0 2px;">NombreLibro</h4></div>
+    <div style="width: 25%; text-align: center; margin: 0 2px; display: flex; align-items: center;"><h4>Categoria</h4></div>
+    <div style="width: 18%; text-align: center; margin: 0 2px; display: flex; align-items: center;"><h4>Precio$</h4></div>
+    <div style="width: 18%; text-align: end;margin: 0 4px; display: flex; align-items: center;"></div>
+    <div style="width: 7%;; margin-right: 4px; display: flex; justify-content: space-around; align-items: center;">
+        <i onclick="editarProductoAdmin('${id}')" class="fa-solid fa-pen" style="color:#06283D; font-size: 15px;"></i>
+        <i onclick="" class="fa-solid fa-trash" style="color:#06283D; font-size: 15px;"></i>
+    </div>
+    `
+}
+
+
 
 //parte de josue
 function inicioSesionMotorista(){
